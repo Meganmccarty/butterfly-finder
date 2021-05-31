52b8fe56e37867296d35752b3c2d4ae7f757f157
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-30 23:22:21 -0400

    Render data into HTML

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const form = document.getElementById('form');
 const stateDropdown = document.getElementById('state-dropdown');
 const taxonSearch = document.getElementById('taxon-search');
+const row = document.querySelector('div.row');
 
 form.addEventListener('submit', submitForm);
 
@@ -18,7 +19,45 @@ function submitForm(e) {
             console.log("Oops! The search term you entered did not turn up any butterflies. Please try searching for another butterfly.");
         } else {
             data.results.map(taxon => {
-                console.log(taxon);
+                // Variables needed for new HTML elements
+                const cardBox = document.createElement('div');
+                const card = document.createElement('section');
+                const img = document.createElement('img');
+                const cardBody = document.createElement('article');
+                const cardText = document.createElement('p');
+                const cardSubBody = document.createElement('div');
+                const button = document.createElement('button');
+                const smallText = document.createElement('small');
+
+                cardBox.classList.add('card-box', 'col-md-4');
+                card.classList.add('card', 'mb-4', 'box-shadow');
+                img.className = 'card-img-top';
+                cardBody.className = 'card-body';
+                cardText.className = 'card-text';
+                cardSubBody.className = 'card-sub-body';
+                button.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+                button.innerText = 'More Info';
+                smallText.className = 'text-muted';
+
+                smallText.innerText = taxon.observed_on;
+                cardSubBody.appendChild(button);
+                cardSubBody.appendChild(smallText);
+
+                cardText.innerText = taxon.taxon.name;
+                cardBody.appendChild(cardText);
+                cardBody.appendChild(cardSubBody);
+
+                const squareImg = taxon.photos[0].url;
+                const largeImg = squareImg.replace('square', 'large');
+
+                img.src = largeImg;
+                card.appendChild(img);
+                card.appendChild(cardBody);
+
+                cardBox.appendChild(card);
+
+                row.appendChild(cardBox);
+                
             })
         }
     })
